15a78c5ba590d26c59b96372e8706be5aeacfc9e
Cyanobacterium <github.com/cyanobacteruim>
2014-08-20 10:08:01 -0400

    Added documentation and vertex access methods

--- src/hall/collin/christopher/stl4j/Triangle.java
+++ src/hall/collin/christopher/stl4j/Triangle.java
@@ -25,15 +25,22 @@ SOFTWARE.
 
 package hall.collin.christopher.stl4j;
 
+import java.util.Arrays;
+
 
 /**
- *
+ * This object represents a triangle in 3D space.
  * @author CCHall
  */
 public class Triangle {
-	final Vec3d[] vertices;
-	final Vec3d normal;
-	
+	private final Vec3d[] vertices;
+	private final Vec3d normal;
+	/**
+	 * Creates a triangle with the given vertices at its corners.
+	 * @param v1 A corner vertex
+	 * @param v2 A corner vertex
+	 * @param v3 A corner vertex
+	 */
 	public Triangle(Vec3d v1, Vec3d v2, Vec3d v3){
 		vertices = new Vec3d[3];
 		vertices[0] = v1;
@@ -43,7 +50,19 @@ public class Triangle {
 		Vec3d edge2 = v3.sub(v1);
 		normal = Vec3d.cross(edge1, edge2).normalize();
 	}
-	
+	/**
+	 * Moves the triangle in the X,Y,Z direction
+	 * @param translation A vector of the delta for each coordinate.
+	 */
+	public void translate(Vec3d translation){
+		for(int i = 0; i < vertices.length; i++){
+			vertices[i] = vertices[i].add(translation);
+		}
+	}
+	/**
+	 * @see java.lang.Object#toString() 
+	 * @return A string that provides some information about this triangle
+	 */
 	@Override public String toString(){
 		StringBuilder sb = new StringBuilder();
 		sb.append("Triangle[");
@@ -52,5 +71,51 @@ public class Triangle {
 		}
 		sb.append("]");
 		return sb.toString();
+	}
+	/**
+	 * Gets the vertices at the corners of this triangle
+	 * @return An array of vertices
+	 */
+	public Vec3d[] getVertices(){
+		return vertices;
+	}
+	/**
+	 * Gets the normal vector
+	 * @return A vector pointing in a direction perpendicular to the surface of 
+	 * the triangle.
+	 */
+	public Vec3d getNormal(){
+		return normal;
+	}
+	
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object) 
+	 * @param obj Object to test equality
+	 * @return True if the other object is a triangle whose verticese are the 
+	 * same as this one.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Triangle other = (Triangle) obj;
+		if (!Arrays.deepEquals(this.vertices, other.vertices)) {
+			return false;
+		}
+		return true;
+	}
+	/**
+	 * @see java.lang.Object#hashCode() 
+	 * @return A hashCode for this triangle
+	 */
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 67 * hash + Arrays.deepHashCode(this.vertices);
+		return hash;
 	}
 }
